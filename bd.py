6df22a4f4a0edab2caeb1f2d6ba28951252b2c58
SandraNAguayoN <72764062+SandraNAguayoN@users.noreply.github.com>
2022-07-16 00:14:14 -0500

    Cambios

--- bd.py
+++ bd.py
@@ -5,3 +5,18 @@ def obtener_conexion():
     user = 'root',
     password = '',
     db = 'signos')
+    
+    
+
+import pandas as pd
+import pyodbc 
+
+conn = pymysql.connect(host='localhost',
+    user = 'root',
+    password = '',
+    db = 'signos')
+sql_query = pd.read_sql_query('SELECT * FROM signos_zodiacales'
+                              ,conn)
+
+df = pd.DataFrame(sql_query)
+df.to_csv(r'C:\Users\sandr\exported_data.csv', index = False) 
--- index.py
+++ index.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template, request, redirect, flash
 import controlador_signos
 
+from flask import Response
+
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+import os
+from bd import obtener_conexion
+
+
+
 app = Flask(__name__)
+
+variables = pd.read_csv('C:\\Users\\sandr\\exported_data.csv')
+x =variables['signo_zodiacal']
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
-
-@app.route('/clusters')
-def clusters():
-    return render_template('clusters.html')
-
 
 @app.route("/agregar_signo")
 def formulario_agregar_signo():
@@ -47,6 +63,42 @@ def guardar_signo():
 def signos():
     registros = controlador_signos.obtener_registros()
     return render_template("signos.html", registros=registros)
+
+
+'''@app.route('/clusters')
+def clusters():
+    lnx=np.log(x)
+    plt.plot(lnx)
+    return render_template('clusters.html', name = plt.show())'''
+
+'''@app.route('/clusters')
+def clusters():
+    return render_template('clusters.html')'''
+
+@app.route('/clusters')
+def clusters():
+  lnx=np.log(x)
+  plt.plot(lnx)   
+  plt.savefig('./static/img/new_plot.png')
+  return render_template('clusters.html', name = 'new_plot', url ='./static/img/new_plot.png')
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
